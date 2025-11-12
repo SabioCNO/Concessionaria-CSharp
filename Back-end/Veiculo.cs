995925c0b99295de7fe7bcac47ef65ef798681f5
SabioCNO <fabinhomiranda324@gmail.com>
2025-11-12 13:40:14 -0300

    Create Veiculo.cs

---
+++ Back-end/Veiculo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Atividade04_Concessionaria
+{
+		// Classe que representa um veículo
+		class Veiculos
+		{
+			public string Marca;
+			public string Modelo;
+			public int Ano;
+			public double Preco;
+			public string Status;
+		}
+}
+	
