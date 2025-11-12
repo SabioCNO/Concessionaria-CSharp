b6859ae365bab72ae396bdbcbc055f8baaf3633f
SabioCNO <fabinhomiranda324@gmail.com>
2025-11-12 13:37:07 -0300

    Create Cliente.cs

---
+++ Back-end/Cliente.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Atividade04_Concessionaria
+{
+		// Classe que representa um cliente da concessionária
+		class Cliente
+		{
+			public string Nome;
+	    	public string CPF;
+	    	public string Numero;
+		}
+}
