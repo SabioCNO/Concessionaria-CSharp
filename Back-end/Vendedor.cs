2bea04b7ef95255faa210c695e7b8190fd4fe20e
SabioCNO <fabinhomiranda324@gmail.com>
2025-11-12 13:48:59 -0300

    Create Vendedor.cs

---
+++ Back-end/Vendedor.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Atividade04_Concessionaria
+{
+    // Classe que representa um vendedor
+    class Vendedor
+    {
+        public string Nome;
+        public string ID;
+    }
+}
